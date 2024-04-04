d9a0f2da45baa858cde6e9a80061e96fd55ba489
Hrishikesh <hrishikeshshintre72@gmail.com>
2024-04-04 13:27:57 +0530

    Added first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { useState } from "react";
+import { ListGroup } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const arrayData = [
+  { serialNo: 1, name: "Hrishikesh", age: 21 },
+  { serialNo: 2, name: "Bhushan", age: 18 },
+  { serialNo: 3, name: "Chinmay", age: 26 },
+];
 
 function App() {
+  const [person, setPerson] = useState(arrayData);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      <h2>List of People</h2>
+      <ListGroup variant="flush">
+        {person.map((person) => (
+          <ListGroup.Item key={person.id}>
+            Name : {person.name}, age : {person.age}
+          </ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   );
 }
